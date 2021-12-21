c3d9c2668cecca577b497099f3d4f90fd8cd4c76
Zennn Ganzz <92146721+zennn06@users.noreply.github.com>
2021-12-21 12:38:06 +0700

    Add files via upload

---
+++ plugins/donasi.js
@@ -0,0 +1,12 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
+┌〔 Donasi • Emoney 〕
+├ Axis : 083164243829
+├ Telkomsel : 081379785293
+└────
+`.trim(), '© Zennn', 'Donasi', '.donasi', m)
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
